f67c8afd99f8a370970cdeb650abd5207d02ea4d
jessica_t <jesst5942@gmail.com>
2022-08-07 01:35:02 +0800

    adding wallet adapter

--- src/page/Launch.js
+++ src/page/Launch.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { FC, ReactNode, useMemo } from "react";
 import styles from "../styles/Landing.module.css";
 import { Button } from "antd";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  CoinbaseWalletAdapter,
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
 import { useNavigate } from "react-router-dom";
 
-const Launch = () => {
+import "@solana/wallet-adapter-react-ui/styles.css";
+const network = WalletAdapterNetwork.Devnet;
+
+const Launch = ({ children }) => {
   const navigate = useNavigate();
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(
+    () => [
+      new CoinbaseWalletAdapter(),
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
 
   const handleClick = () => {
     navigate("/game");
@@ -21,6 +57,25 @@ const Launch = () => {
       >
         CONNECT WALLET
       </Button>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                marginRight: "15px",
+                gap: "15px",
+                marginTop: "10px",
+              }}
+            >
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+            </div>
+            {children}
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </div>
   );
 };
